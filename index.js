44418377f9b8b0839f39af9e45d852fee6bab265
wen.you <wen.you@nextfortune.tw>
2022-03-04 11:30:56 +0800

    add score table

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const SCORE_ZERO = 0;
+const TEAM_COUNT = 5;
+const ROUND_COUNT = 5;
+let scoreAry = Array.from({ length: TEAM_COUNT }, () => {
+    return Array.from({ length: ROUND_COUNT }, () => SCORE_ZERO);
+});
+
+console.log(scoreAry);
+
 window.onload = () => {
     const randomBtn = document.querySelector(".random-btn");
     randomBtn.addEventListener("click", topicGet);
+    scoreFormSet();
 };
 
 function getRandomInt(max) {
@@ -16,3 +26,48 @@ function topicGet() {
     const topicValue = `${adj[adjIdx]}${noun[nounIdx]}`;
     topic.innerText = topicValue;
 }
+
+function scoreFormSet() {
+    const scoreThead = document.querySelector(".score thead");
+    const scoreTbody = document.querySelector(".score tbody");
+    let thead = "";
+    let tbody = "";
+    let totalScore = "";
+
+    for (const teamIdx in scoreAry) {
+        let tbodyTmp = "";
+        let scoreTotalTmp = 0;
+        thead += `<th>Team${parseInt(teamIdx) + 1}</th>`;
+        for (const score of scoreAry[teamIdx]) {
+            scoreTotalTmp += score;
+            tbodyTmp += `<td>
+                            <input type="number" value="${score}" />
+                        </td>`;
+        }
+
+        tbody += `<tr>
+                    <td>Round${parseInt(teamIdx) + 1}</td>
+                    ${tbodyTmp}
+                    <td>${scoreTotalTmp}</td>
+                </tr>`;
+    }
+
+    thead = `<thead>
+                <tr>
+                    <th></th>
+                    ${thead}
+                    <th>Total</th>
+                </tr>
+            </thead>`;
+    tbody = `<tbody>${tbody}</tbody>`;
+    scoreThead.innerHTML = thead;
+    scoreTbody.innerHTML = tbody;
+}
+
+function scoreFromLocalGet() {}
+
+function scoreFromLocalSave() {}
+
+function scoreFromLocalClear() {}
+
+function scoreAdd() {}
